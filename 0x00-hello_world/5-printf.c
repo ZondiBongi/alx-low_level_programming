cc8427e908b2ec4002724f37c944b21046f9f4ab
Bongiswa Zondi <zondibongiswa@yaoo.com>
2023-02-19 11:21:28 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  * */
 int main(void)
 {
-	ptintf("with proper grammar, but the outcome is a piece of art,\n");
-	retorn (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
 
