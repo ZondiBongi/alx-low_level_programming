7e69d5909a7f076685806af55d4178a50c5ad0e8
Bongiswa Zondi <zondibongiswa@yaoo.com>
2023-02-20 04:06:59 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
-* main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
-*                followed by a new line, to standard error.
+* main Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+* followed by a new line, to the standard error.
 *
 * Return: Always 1.
 */
