4aff54453fc899ec60ab641fde58a671db44e159
Bongiswa Zondi <zondibongiswa@yaoo.com>
2023-02-22 17:41:17 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int_abs(in) -computes the absolute value of a integer
+ * @c: The integer to be checked
+ * Return: The absolute value of number or zero
+ */
+
+int _abs(int c)
+{
+	if (c <0)
+	{
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
