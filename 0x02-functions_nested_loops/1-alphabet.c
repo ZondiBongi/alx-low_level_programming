1aef7ab0b3c61acbd0a541bdf2c26323110a89c0
Bongiswa Zondi <zondibongiswa@yaoo.com>
2023-02-24 04:37:45 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
-
 /**
  * print_alphabet - Make the alphabet
  * Return: void
  */
 
-void print_alphabet(void)
+void  print_alphabet(void)
 {
 	char c;
 
-	for(c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 	}
