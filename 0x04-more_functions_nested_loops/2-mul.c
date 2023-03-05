b43915e4129b609282d21f38daff336da32c1163
Bongiswa Zondi <zondibongiswa@yaoo.com>
2023-03-05 04:50:31 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
+ * Return: The result of multiplication between a param and b param
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
