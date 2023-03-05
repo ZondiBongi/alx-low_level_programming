d3bbac4cc448edd1d5fac3cfcb1dd4daf42ab04b
Bongiswa Zondi <zondibongiswa@yaoo.com>
2023-03-05 04:35:52 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: the number to checked
+ * Return: 1 for a characer that is a digit or 0 for anything else
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && x <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 0. isupper. Write a function that checks for uppercase character.Returns 1 if c is uppercase.Returns 0 otherwise.
+1.Write a function that checks for a digit (0 through 9).
